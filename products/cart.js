ec646cd66f4a54ab10aa4731d5b1ff8a92006288
SleepyDreams-TB <102594100+SleepyDreams-TB@users.noreply.github.com>
2025-10-01 11:36:23 +0200

    Update cart.js to0 add payment key url

--- products/cart.js
+++ products/cart.js
@@ -10,7 +10,7 @@ export function clearCart() {
 
 export async function createPayment(payment_type, amount, reference) {
   try {
-    const res = await fetch('http://localhost:3000/api/payment', {
+    const res = await fetch('https://services.callpay.com/api/v2/payment-key', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({

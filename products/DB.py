c43e9ed4fee208a81f9a8a5b6c0ea5638fc79e65
SleepyDreams-TB <tiaanburger1112@gmail.com>
2025-10-02 22:33:57 +0200

    uvicorn run fix

--- products/DB.py
+++ products/DB.py
@@ -291,4 +291,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("products.productsDB:app", host="0.0.0.0", port=port)
+    uvicorn.run("products.DB:app", host="0.0.0.0", port=port)

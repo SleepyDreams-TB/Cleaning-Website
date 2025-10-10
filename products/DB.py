ba9ce1f600667b7d8a5376f230e41bf87343141e
SleepyDreams-TB <102594100+SleepyDreams-TB@users.noreply.github.com>
2025-10-10 09:48:34 +0200

    origins fix

--- products/DB.py
+++ products/DB.py
@@ -31,7 +31,7 @@ origins = ["https://kingburger.site"]
 # --------- CORS middleware ---------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origins],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
